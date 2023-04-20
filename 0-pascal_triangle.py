af372f88b16efaa081e94e55e2e758dadeafa226
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-04-20 22:53:16 +0100

    Update 0-pascal_triangle.py

--- 0-pascal_triangle.py
+++ 0-pascal_triangle.py
@@ -1,6 +1,21 @@
 #!/usr/bin/python3
 """
-0-main
+pascal's triangle
 """
 
+from math import factorial
+
 def pascal_triangle(n):
+    """function that returns a list of lists"""
+    l = []
+    a = []
+    if n <= 0:
+        return [[]]
+    for i in range(n):
+	    for j in range(i+1):
+		  # nCr = n!/((n-r)!*r!)
+		    l.append(factorial(i)//(factorial(j)*factorial(i-j)))
+	    a.append(l)
+	    l = []
+    return a
+  
