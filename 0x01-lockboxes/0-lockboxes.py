2330dc206ac543be8e2b2ed2fca50f2da448d8df
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-04-25 09:11:50 +0100

    Update 0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -7,8 +7,6 @@ lockboxes
 def canUnlockAll(boxes):
     """Return True if all boxes can be opened, else return False"""
     l = boxes[0]
-    if l == [[]] or l == []:
-        return False
     l.append(0)
     for i in l:
         if i < len(boxes):
