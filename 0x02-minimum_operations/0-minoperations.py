218da1feef28b9e29dcff1ac23d6eb323c574690
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-05-03 12:01:46 +0100

    Update 0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -11,12 +11,22 @@ def minOperations(n):
     s = 'HH'
     sum = 2
     lm = []
-    for i in range(1, n + 1):
-        if n % i == 0 and i < n // 2:
-            lm.append(i)
-    a = max(lm) - 2
-    s = s + ('H' * a)
-    sum += len(s) - len('HH')
-    sum += 2
-    b = (n / len(s)) - 2
-    return int(b + sum)
+    h = n / 2
+    if n % 2 == 0:
+        for i in range(1, n + 1):
+            if n % i == 0 and i < n // 2:
+                lm.append(i)
+        b = lm[2]
+        c = b - len(s)
+        k = (h / b) - 2
+        sum = sum + 2 + c + k + 2
+        return int(sum)
+    else:
+        for i in range(1, n + 1):
+            if n % i == 0 and i < n // 2:
+                lm.append(i)
+        b = lm[1]
+        c = b - len(s)
+        k = (h / b) - 2
+        sum = sum + 2 + c + k + 2
+        return int(sum)
