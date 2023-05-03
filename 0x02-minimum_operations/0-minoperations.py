9de40827cfb53d2d88db27b77bdd10b8ae20bb6d
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-05-03 12:26:03 +0100

    Update 0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -14,11 +14,14 @@ def minOperations(n):
     h = n / 2
     if n % 2 == 0:
         for i in range(1, n + 1):
-            if n % i == 0 and i < n // 2:
+            if n % i == 0 and i <= n // 2:
                 lm.append(i)
-        b = lm[2]
-        c = b - len(s)
-        k = (h / b) - 2
+                if i != 1 and i != 2:
+                    break
+        c = lm[-1] - len(s) # no of pastes 0
+        if lm[-1] == h:
+            return sum + 2 + c
+        k = (h / lm[-1]) - 2 # no of cp
         sum = sum + 2 + c + k + 2
         return int(sum)
     else:
