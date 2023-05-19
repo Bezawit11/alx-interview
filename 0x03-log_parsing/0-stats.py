62218a1b53faf8e2b3c0436c74f010f4b451ba0a
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-05-19 09:59:58 +0100

    Update 0-stats.py

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""log parsing"""
+
+
+import sys
+ 
+ l = []
+ d = {'200': 0,
+      '401': 0,
+      '403': 0,
+      '404': 0,
+      '405': 0,
+      '500': 0
+ }
+sum = 0
+for line in sys.stdin:
+    l.append(line)
+    if len(l) == 10:
+        break
+for i in l:
+    sum += int(i.split()[-1])
+    key = i.split()[-2]
+    if key in d.keys():
+        d[key]+= 1
+print('File size: sum')
+for k,v in d.items():
+    print(k, v)
