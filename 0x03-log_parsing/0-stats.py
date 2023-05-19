ff842ce80d94c852767b5c52681b62b4aa7fe98e
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-05-19 12:36:32 +0100

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -7,26 +7,27 @@ import errno
  
 l = []
 d = {'200': 0,
-      '401': 0,
-      '403': 0,
-      '404': 0,
-      '405': 0,
-      '500': 0
+     '301': 0,
+     '400': 0,
+     '401': 0,
+     '403': 0,
+     '404': 0,
+     '405': 0,
+     '500': 0
  }
 sum = 0
-for line in sys.stdin:
-    l.append(line)
-    if len(l) == 10:
-        break
-for i in l:
-    sum += int(i.split()[-1])
-    key = i.split()[-2]
-    if key in d.keys():
-        d[key]+= 1
-print('File size: sum')
 try:
+    for line in sys.stdin:
+        l.append(line)
+        if len(l) == 10:
+            break
+    for i in l:
+        sum += int(i.split()[-1])
+        key = i.split()[-2]
+        if key in d.keys():
+            d[key]+= 1
+    print('File size: sum')
     for k,v in d.items():
         print(k, v)
-except (BrokenPipeError, IOError):
-    print ('BrokenPipeError caught', file = sys.stderr)
-sys.stderr.close()
+finally:
+    print_msg(d, sum)
