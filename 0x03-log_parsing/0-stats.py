1fda66ba14063122384e2c14243ee620221f15f6
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-05-20 12:36:26 +0100

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -4,36 +4,36 @@
 
 import sys
  
-
-l = []
-d = {'200': 0,
-     '301': 0,
-     '400': 0,
-     '401': 0,
-     '403': 0,
-     '404': 0,
-     '405': 0,
-     '500': 0
- }
-sum = 0
-try:
-    for line in sys.stdin:
-        l.append(line)
-        if len(l) == 10:
-            break
-    for i in l:
-        sum += int(i.split()[-1])
-        key = i.split()[-2]
-        if key in d.keys():
-            d[key]+= 1
-    print('File size:', sum)
-    for k,v in d.items():
-        if v != 0:
-            print("{}: {}".format(k, v))
-except KeyboardInterrupt:
-    print('File size:', sum)
-    for k,v in d.items():
-        if v != 0:
-            print("{}: {}".format(k, v))
-    raise
+if __name__ == '__main__':
+    l = []
+    d = {'200': 0,
+         '301': 0,
+         '400': 0,
+         '401': 0,
+         '403': 0,
+         '404': 0,
+         '405': 0,
+         '500': 0
+     }
+    sum = 0
+    try:
+        for line in sys.stdin:
+            l.append(line)
+            if len(l) == 10:
+                break
+        for i in l:
+            sum += int(i.split()[-1])
+            key = i.split()[-2]
+            if key in d.keys():
+                d[key]+= 1
+        print('File size:', sum)
+        for k,v in d.items():
+            if v != 0:
+                print("{}: {}".format(k, v))
+    except KeyboardInterrupt:
+        print('File size:', sum)
+        for k,v in d.items():
+            if v != 0:
+                print("{}: {}".format(k, v))
+        raise
 
