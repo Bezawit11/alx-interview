b4bcf5d90e58831e149162ee3111ce3ef6db4f45
Bezawit11 <socbeza13@gmail.com>
2023-06-12 12:34:43 +0000

    task 0

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+0x07 - Rotate 2D Matrix
+"""
+def rotate_2d_matrix(m):
+    """rotates a 2d matrix by 90 degrees"""
+    a = []
+    o = []
+    f = m.copy()
+    m.clear()
+    for i in range(3):
+        for n in range(3):
+            o.append(f[n][i])
+        m.append(o)
+        o = []
+    g = m.copy()
+    m.clear()
+    for j in g:
+        j = j[::-1]
+        m.append(j)
